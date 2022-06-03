a2fa7a351baa325f251b359c698d22ada563a6aa
Your Name <you@example.com>
2022-06-03 20:37:56 +0200

    added the first test and its counterpart in the libary, the split function

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,34 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+
+use regex::Regex;
+pub fn split(input: String) -> Vec<String> {
+    let mut return_vec: Vec<String> = Vec::new();
+
+    for mat in Regex::new(r#""[^"]+"|'[^']+'|[\w'-'/]+"#)
+        .unwrap()
+        .find_iter(input.as_str())
+    {
+        let  to_append = mat.as_str();
+
+     
+
+     if to_append.starts_with('\'') && to_append.ends_with('\'')|| to_append.starts_with('"') && to_append.ends_with('"'){
+            let mut chars = to_append.chars();
+            chars.next();
+            chars.next_back();
+            let to_return = chars.as_str();
+            return_vec.push(to_return.to_string());
+
+
+        }
+        else {
+            return_vec.push(to_append.to_string());
+
+        }
+
     }
+    return_vec
+
 }
+
+
+
