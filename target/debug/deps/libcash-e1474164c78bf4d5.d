0561d02e5150301f7cf768607fcf3d95315ac77c
Your Name <you@example.com>
2022-06-05 18:27:25 +0200

    the algorithm works

--- target/debug/deps/libcash-e1474164c78bf4d5.d
+++
@@ -1,5 +0,0 @@
-/data/data/com.termux/files/home/libcash/target/debug/deps/libcash-e1474164c78bf4d5: src/lib.rs
-
-/data/data/com.termux/files/home/libcash/target/debug/deps/libcash-e1474164c78bf4d5.d: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/splitting_whitespaces-3cd94a12a083d3ff.d
+++
@@ -1,5 +0,0 @@
-/data/data/com.termux/files/home/libcash/target/debug/deps/splitting_whitespaces-3cd94a12a083d3ff: tests/splitting_whitespaces.rs
-
-/data/data/com.termux/files/home/libcash/target/debug/deps/splitting_whitespaces-3cd94a12a083d3ff.d: tests/splitting_whitespaces.rs
-
-tests/splitting_whitespaces.rs:
