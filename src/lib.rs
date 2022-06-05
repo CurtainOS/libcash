a20bfef95b846e6b3343b137fc0c966701739ec4
Your Name <you@example.com>
2022-06-05 18:47:55 +0200

    removed all dependencies rewrote the entire code and optimized it. not yet flawless, but at least editable

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,50 @@
 
-use regex::Regex;
-use std::io;
-pub fn split(input: String) -> Result<Vec<String>, io::Error> {
+pub fn split(input: String) -> Result<Vec<String>, std::io::Error> {
     let mut return_vec: Vec<String> = Vec::new();
+    let mut temporary_vec: Vec<String> = Vec::new();
+    let mut is_this_in_quotes = false;
 
-    for mat in Regex::new(r#""[^"]+"|'[^']+'|[\w'-'/'.]+"#)
-        .unwrap()
-        .find_iter(input.as_str())
-    {
-        let  to_append = mat.as_str();
-
-     
-
-     if to_append.starts_with('\'') && to_append.ends_with('\'')|| to_append.starts_with('"') && to_append.ends_with('"'){
-            let mut chars = to_append.chars();
-            chars.next();
-            chars.next_back();
-            let to_return = chars.as_str();
-            return_vec.push(to_return.to_string());
-
-
-        }
-        else {
-            return_vec.push(to_append.to_string());
+    for i in input.split_whitespace() {
+        if is_this_in_quotes {
+            if i.ends_with('"') {
+                is_this_in_quotes = false;
+                let last_off = &i[0..i.len() - 1];
+                temporary_vec.push( " ".to_owned() + last_off);
+                let to_append = vec_to_string(&temporary_vec);
+                println!("{}", to_append);
+                return_vec.push(to_append);
+                temporary_vec.clear();
+                
+            } else {
+                let return_str = " ".to_owned() + i;
+                temporary_vec.push(return_str);
+            }
 
         }
+        else if i.starts_with('"') {
+                is_this_in_quotes = true;
+                let first_off: &str = &i[1..i.len()];
+                temporary_vec.push(first_off.to_owned());
+                continue
+
+            } else {
+                return_vec.push(i.to_owned());
+            }
+        
 
     }
-    Ok(return_vec)
+
+
+
+fn vec_to_string(input: &Vec<String>) -> String {
+    let mut return_string = String::new();
+    for i in input {
+        return_string.push_str(i);
+  }
+    return_string
+}
+   Ok(return_vec)
+ 
 
 }
 
